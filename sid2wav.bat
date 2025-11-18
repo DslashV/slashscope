708327cd2d5dd039fad350d32c7da6324f5f344d
DslashV <danielveronski@gmail.com>
2025-11-18 21:43:04 +0200

    added support for sid files that include PCM samples (6581 only)

--- sid2wav.bat
+++ sid2wav.bat
@@ -10,7 +10,8 @@ if not exist sidplayfp.exe (
 	pause
 )
 
-sidplayfp -u2 -u3 -w%output%chan1.wav %sidfile%
-sidplayfp -u1 -u3 -w%output%chan2.wav %sidfile%
-sidplayfp -u1 -u2 -w%output%chan3.wav %sidfile%
+sidplayfp -u2 -u3 -g1 -w%output%chan1.wav %sidfile%
+sidplayfp -u1 -u3 -g1-w%output%chan2.wav %sidfile%
+sidplayfp -u1 -u2 -g1 -w%output%chan3.wav %sidfile%
+sidplayfp -u1 -u2 -u3 -w%output%chanpcm.wav %sidfile%
 sidplayfp -w%output%chanmas.wav %sidfile%
