050f689fb62f0df1cf93530187ed3b8eff823e58
D/V <128831268+DslashV@users.noreply.github.com>
2026-04-04 21:44:39 +0300

    Enhance sid2wav.bat with user-defined sidplayfp path
    Updated the script to allow user-defined path for sidplayfp executable and added an option to disable samples.
    
    
    github ai thx

--- sid2wav.bat
+++ sid2wav.bat
@@ -5,16 +5,22 @@ echo type the output file
 set /p output=
 echo type in the length of the sid (in seconds only)
 set /p timesec=
+echo put in the sidplayfp exe file here:
+set /p sidplayfpexe=
+echo you want to disable samples? (useful if you want to hear SIDs with the echo routine) (Y for yes, N for no)
+set /p smpen=
 
-if not exist sidplayfp.exe (
-	echo could not find sidplayfp
-	echo you must place sidplayfp in your path or in the same folder as the bat file
-	pause
-)
-    
-    
-C:\sidplayfp\sidplayfp.exe -u2 -u3 -g1 -t%timesec% -w%output%chan1sid1.wav %sidfile%
-C:\sidplayfp\sidplayfp.exe -u1 -u3 -g1 -t%timesec% -w%output%chan2sid1.wav %sidfile%
-C:\sidplayfp\sidplayfp.exe -u1 -u2 -g1 -t%timesec% -w%output%chan3sid1.wav %sidfile%
-C:\sidplayfp\sidplayfp.exe -u1 -u2 -u3 -t%timesec% -w%output%chanpcmsid1.wav %sidfile%
-C:\sidplayfp\sidplayfp.exe -t%timesec% -w%output%chanmassid1.wav %sidfile%
+if %smpen%=="N" {
+	%sidplayfpexe% -u2 -u3 -t%timesec% -w%output%chan1sid1.wav %sidfile%
+	%sidplayfpexe% -u1 -u3 -t%timesec% -w%output%chan2sid1.wav %sidfile%
+	%sidplayfpexe% -u1 -u2 -t%timesec% -w%output%chan3sid1.wav %sidfile%
+	%sidplayfpexe% -u1 -u2 -t%timesec% -w%output%chanpcmsid1.wav %sidfile%
+	%sidplayfpexe% -t%timesec% -w%output%chanmassid1.wav %sidfile%
+} else {
+	%sidplayfpexe% -u2 -u3 -g1 -t%timesec% -w%output%chan1sid1.wav %sidfile%
+	%sidplayfpexe% -u1 -u3 -g1 -t%timesec% -w%output%chan2sid1.wav %sidfile%
+	%sidplayfpexe% -u1 -u2 -g1 -t%timesec% -w%output%chan3sid1.wav %sidfile%
+	%sidplayfpexe% -u1 -u2 -u3 -t%timesec% -w%output%chanpcmsid1.wav %sidfile%
+	%sidplayfpexe% -t%timesec% -w%output%chanmassid1.wav %sidfile%
+}
+goto :eof
